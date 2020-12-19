881c02e45bb5115c11b37aff519a3ac81c2cc69f
jtaylor6 <jarrede20@gmail.com>
2020-12-19 00:58:36 -0500

    created a stock e-com display to work from

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:shopping_bot/constants.dart';
+import 'package:shopping_bot/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/back.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/search.svg",
+            // By default the  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset(
+            "assets/icons/cart.svg",
+            // By default the  icon color is white
+            color: kTextColor,
+          ),
+          onPressed: () {},
+        ),
+        SizedBox(width: kDefaultPaddin / 2)
+      ],
+    );
+  }
+}
